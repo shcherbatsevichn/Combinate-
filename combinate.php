3633040afed9bd79533f45bbe00417e29230a6cc
shcherbatsevichn <shcherbatsevich.n.dvl@yandex.by>
2022-01-18 10:11:15 +0300

    combinate-fix

--- combinate.php
+++ combinate.php
@@ -4,21 +4,33 @@ class Combinate{
 
     public $data;
     public $length;
+    public $counter;
+    public $counterm;
+    public $resarray;
+    public $resultnumber;
+    public $resultnumberprint;
+    public $mainnumber;
 
     public function __construct($inputdata, $linelength) 
     {
         $this->data = $inputdata;
         $this->length = $linelength;
+        $this->counter = 0;
+        $this->counterm = 0;
+        $this->resarray = [str_split($this->data)];
+        $this->resultnumber = "";
+        $this->resultnumberback = [];
+        $this->mainnumber = 0;
     }
 
-    public function get_factorial($n){  // n!
+    public function getFactorial($n){  // n!
         if($n <= 1){
             return 1;
         }
-        return $n*$this->get_factorial($n-1);
+        return $n*$this->getFactorial($n-1);
     }
 
-    public function number_of_stirring(){ //метод который считает размещения, в обход основного алгоритма.
+    public function numberOfStirring(){ //метод который считает размещения, в обход основного алгоритма.
         $nm = (strlen($this->data) - $this->length);
         if($nm > strlen($this->data)){
             throw new Exception('Неверно указанно множество или подмножество. Невалидные данные были заменены на 1.<br>');
@@ -26,13 +38,13 @@ class Combinate{
         if($nm < 0){
             throw new Exception('Указанное подмножество больше основного множества! Данные заменены на длинну множества<br>');
         }
-        $nfact = $this->get_factorial(strlen($this->data));
-        $nnmfact = $this->get_factorial($nm);
+        $nfact = $this->getFactorial(strlen($this->data));
+        $nnmfact = $this->getFactorial($nm);
         $res = $nfact / $nnmfact;
         return $res;
     }
     
-    public function delete_element($index, $array){ //удаление элемента из массива, возвращает массив без элемента
+    public function deleteElement($index, $array){ //удаление элемента из массива, возвращает массив без элемента
         $arr = $array;
         $res = [];
         $n = 0;
@@ -46,7 +58,7 @@ class Combinate{
         return $res;
     }
 
-    public function get_element($index, $array){ // возвращяет указанный элемент из массива 
+    public function getElement($index, $array){ // возвращяет указанный элемент из массива 
         $arr = $array;
         for($i = 0; $i < count($arr); $i++){
             if($i == $index){
@@ -54,43 +66,25 @@ class Combinate{
             }
         }
     }
-    public function get_combination($array = '', $counter = '', $res = ''){ // создаёт комбинации
-        if(!is_array($array)){   // если значение не переданно, устанавливаем указанное значение 
-            $array = str_split($this->data);
-        }
-        if($counter === ''){    // если не указан, устанавливаем стартовое значение
-            $counter = $this->length - 2;
-        }
-        if($this->length - 2 < 0){  // если необходимо выводить подмножество, с длинной 1
-            for($j = 0; $j < count($array); $j++){
-                echo($res.$array[$j]."<br>"); 
+    public function getCombination(){ // создаёт комбинации 
+        for($i = 0; $i < count($this->resarray[$this->counterm]); $i++){
+            $this->mainnumber = $this->getElement($i, $this->resarray[$this->counterm]);
+            $this->resarray[$this->counterm + 1] = $this->deleteElement($i, $this->resarray[$this->counterm]);
+            $this->resultnumber = $this->resultnumber.$this->mainnumber;
+            if (strlen($this->resultnumber) == $this->length){
+                echo($this->resultnumber."<br>");
             }
-            exit;
-        }
-        for($i = 0; $i < count($array); $i++){
-            $mainnumb = $this->get_element($i, $array); // получаем одну цифру "Главную" в этойитерации. 
-            $arr = $this->delete_element($i, $array); //получаем массив без "главной" цифры 
-            $res1 = $res.$mainnumb; // получаем "замароженный результат для вывода его на нужной глубине рекурсии
-            if($counter > 0){  // входим в рекурсию, если необходимо 
-                $counter--;  // регулируем глубину рекурсии
-                $this->get_combination($arr, $counter, $res1); 
-            }else{              // если мы на нужной глубине
-                for($e = 0; $e < count($arr); $e++){        //перебираем 
-                    $res2 = $res.$mainnumb.$arr[$e];
-                    if(strlen($res2) == $this->length){
-                        echo($res2."<br>");
-                        $counter--;
-                    }else{
-                        $arres = $this->delete_element($e, $arr);
-                        $res2 = $res2.$arres[$e];  
-                        if(strlen($res2) == $this->length){
-                            echo($res2."<br>");
-                            $counter--;
-                        } 
-                    }
-                } 
+            $this->counter++;
+            $this->counterm++;
+            if($this->counter < $this->length){ 
+                $this->getCombination();
             }
-            $counter++;
-        } 
+            $this->resultnumberback = str_split($this->resultnumber);
+            $this->resultnumber = implode($this->deleteElement(count($this->resultnumberback) - 1, $this->resultnumberback));
+            $this->counterm--;
+            $this->counter--;
+        }
+        $this->counter--;
     }
 }
+      
--- index.php
+++ index.php
@@ -5,20 +5,20 @@ ini_set('display_startup_errors', 1);
 
 require_once "combinate.php";
 
-$number = 1234;
-$len = 3;
+$number = 12345;
+$len = 2;
 
 $r = new Combinate($number, $len);
 
 try{
-    $n = $r->number_of_stirring();
+    $n = $r->numberOfStirring();
     echo("Количество возможных комбинаций: ");
     echo($n."<br>");
 }catch (Exception $e) {
     if($e->getMessage() == 'Неверно указанно множество или подмножество. Невалидные данные были заменены на 1.<br>'){
         echo 'Возникла Ошибка: ',  $e->getMessage(), "\n";
         $r->length = 1;
-        $n = $r->number_of_stirring();
+        $n = $r->numberOfStirring();
         echo("Количество возможных комбинаций: ");
         echo($n."<br>");
     }
@@ -26,9 +26,9 @@ try{
         echo 'Возникла Ошибка: ',  $e->getMessage(), "\n";
         $len = strlen($r->data);
         $r->length = $len;
-        $n = $r->number_of_stirring();
+        $n = $r->numberOfStirring();
         echo("Количество возможных комбинаций: ");
         echo($n."<br>");
     }
 }
-$r->get_combination();
+$r->getCombination();
